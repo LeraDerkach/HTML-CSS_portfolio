215d64a4e5b30967adabf148a41cafd0e6e0351f
LeraDerkach <lera1derkach@gmail.com>
2026-03-11 11:40:03 +0200

    Add mobile navigation functionality to main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ close.addEventListener('click', () => {
     menu.classList.remove('active');
     burger.classList.remove('active');
 });
+
+let btn = document.querySelector(".navbar-btn-mob");
+let navMenu = document.querySelector(".navbar-menu-mob");
+let closeBtn = document.querySelector(".navbar-close-btn");
+
+btn.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    btn.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    btn.classList.remove('active');
+});
